e6d6667d2d4d5e3c135ab70329fa75a9bd32da39
ablache-hub <alexandreblache.pro@gmail.com>
2022-04-18 19:34:19 +0200

    filter

--- main.js
+++ main.js
@@ -6,20 +6,31 @@ import {
 // Lecture et parsing en JSON du fichier texte
 const movieList = JSON.parse(fs.readFileSync('./src/localAPIModel.txt'))
 
-// Test de récup. et affichage de données du fichier
-console.log(movieList.map(
-    movie => {
-        return {
-            title: movie.Title,
-            release_date: movie.Released,
-            runtime: timeConvert(movie.Runtime),
-            genre: movie.Genre,
-            synopsys: movie.Plot.slice(0, 100) + '...',
-            director: movie.Director,
-            actors: movie.Actors,
-            rating: movie.imdbRating + "/10",
-            poster: movie.Poster,
-            type: movie.Type
+
+
+// Exemple de récup. et affichage de données du fichier
+// console.log(
+    movieList.map(
+        movie => {
+            return {
+                title: movie.Title,
+                release_date: movie.Released,
+                runtime: timeConvert(movie.Runtime),
+                genre: movie.Genre,
+                synopsys: movie.Plot.slice(0, 100) + '...',
+                director: movie.Director,
+                actors: movie.Actors,
+                rating: movie.imdbRating + "/10",
+                poster: movie.Poster,
+                type: movie.Type
+            }
         }
-    }
-))
+    )
+// )
+
+// Exemple de recherche dans l'array avec .filter()
+// console.log(
+    movieList.filter(function (e) {
+        return e.Title == 'Howard the Duck'
+    })
+// );
